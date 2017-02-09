a072a4811d560436d9ec658a9b4a1a0821f1218a
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-02-08 22:42:02 -0800

    Add stack methods in functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,15 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var newInstance = {};
+  var newInstance = {
+    stackSize: 0
+  };
 
   _.extend(newInstance, stackMethods);
-  
+
   return newInstance;
 };
 
 var stackMethods = {
   size: function() {
-    return 0;
+    return this.stackSize;
+  },
+  push: function() {
+    this.stackSize++;
+  },
+  pop: function() {
+    if (this.stackSize > 0) {
+      this.stackSize--;
+    }
   }
 };
