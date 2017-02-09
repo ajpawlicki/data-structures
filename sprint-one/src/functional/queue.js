99d1280a7c635a457f102588ef7c38b7d7c95af9
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-02-08 22:07:57 -0800

    Fix queue so least recently added of two items is removed and returned

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,10 +8,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
     size++;
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      // debugger;
+      size--;
+      var result = storage[0];
+      delete storage[0];
+      // Shift keys in object down by 1
+      for (var i = 0; i < size; i++) {
+        for (var key in storage) {
+          storage[i] = storage[key];
+        }
+      }
+      return result;
+    }
   };
 
   someInstance.size = function() {
