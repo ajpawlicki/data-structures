caf0758e271229df534a707a60a833113d71ce0f
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-02-08 23:53:14 -0800

    Add size and enqueque functions for functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,20 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = {
+    queueSize: 0
+  };
+  _.extend(newInstance, queueMethods);
+  return newInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.queueSize;
+  },
+  enqueue: function() {
+    this.queueSize++;
+  }
+};
 
 
