363dce43b3dab86873033a6c41129e635460caf8
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-02-10 18:22:31 -0800

    Finish linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (this.head === null && this.tail === null) {
+      var firstNode = Node(value);
+      this.tail = firstNode;
+      this.head = firstNode;
+    } else {
+      var newNode = Node(value);
+      this.tail.next = newNode;
+      this.tail = this.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    var formerHead = this.head.value;
+    this.head = this.head.next;
+    return formerHead;
   };
 
   list.contains = function(target) {
+    // base case
+    var checkNode = function(node) {
+      if (node.value === target) {
+        // debugger;
+        return true;
+      } else if (node.next === null) {
+        return false;
+      } else {
+        return checkNode(node.next);
+      }
+    }
+
+    return checkNode(this.head);
+
+    // return _.contains(this.head.value, target);
   };
 
   return list;
